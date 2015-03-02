7fafd65b433f9b78dffa56afae10a5ddd0ff5897
marti-org <martinorava@seznam.cz>
2015-03-03 00:11:53 +0100

    Have to change absolute size

--- ClassWindows/TextStoryApp/TextStoryApp/MainPage.xaml.cs
+++ ClassWindows/TextStoryApp/TextStoryApp/MainPage.xaml.cs
@@ -19,25 +19,11 @@ using Windows.Storage.Streams;
 using Windows.UI.Xaml.Media.Imaging;
 
 using Windows.UI.Popups;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
-using System;
 
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
-using System;
-using System.Linq;
-using System.Collections.Generic;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+
 using Windows.Graphics.Display;
 using Windows.UI.ViewManagement;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+
 using Windows.Security.Credentials.UI; 
 
 
@@ -50,34 +36,101 @@ namespace TextStoryApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        
+
+        private bool lastImgOnLeft = false;
+
+        Grid newGrid;
+
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-        private async void btnInsetPicture_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// This method create new grit 4x4
+        /// </summary>
+        private void createNewGrid()
         {
             
+            // Create the Grid
+            newGrid = new Grid();
+            newGrid.HorizontalAlignment = HorizontalAlignment.Left;
+            newGrid.VerticalAlignment = VerticalAlignment.Top;
+
+            // Define the Columns
+            ColumnDefinition colDef1 = new ColumnDefinition();
+            ColumnDefinition colDef2 = new ColumnDefinition();
+
+            newGrid.ColumnDefinitions.Add(colDef1);
+            newGrid.ColumnDefinitions.Add(colDef2);
+
+            // Define the Rows
+            RowDefinition rowDef1 = new RowDefinition();
+            RowDefinition rowDef2 = new RowDefinition();
+
+            rowDef1.Height = new GridLength(0.8, GridUnitType.Star);
+            rowDef2.Height = new GridLength(0.2, GridUnitType.Star);
+            newGrid.RowDefinitions.Add(rowDef1);
+            newGrid.RowDefinitions.Add(rowDef2);
+        }
+
+
+        private async void btnInsetPicture_Click(object sender, RoutedEventArgs e)
+        {
+
+            int gridCollumToInsert = 1;//Select columt to insert photo and text
+                                        //0 for left colum, 1 to right colum
+
+            //Create new grid if is inserted 2 photo
+            if (!lastImgOnLeft)
+            {
+                createNewGrid();
+                gridCollumToInsert = 0;
+            }
+
+            //Create photo using camera
             CameraCaptureUI capture = new CameraCaptureUI();
             capture.PhotoSettings.CroppedAspectRatio = new Size(16, 9);
             StorageFile file = await capture.CaptureFileAsync(CameraCaptureUIMode.Photo);
             IRandomAccessStream filestream = await file.OpenAsync(FileAccessMode.Read);
             BitmapImage img = new BitmapImage();
             img.SetSource(filestream);
+
+            Image img_viewer = new Image();
             img_viewer.Source = img;
-            MessageDialog dlg = new MessageDialog("Message"); await dlg.ShowAsync();
             
+            //Insert photo into grid
+            img_viewer.SetValue(Grid.ColumnProperty, gridCollumToInsert);
+            img_viewer.SetValue(Grid.RowProperty, 0);
+            newGrid.Children.Add(img_viewer);
 
-            /*
-            string message, title, defaultValue;
-            string myValue;
-            message = "Enter Message Here :";
-            title = "Title Name";
-            myValue = Interaction.InputBox(message, title, defaultValue, 450, 450);
-            */
+            //Get text to photo description
+            InputMessageDialog dlg = new InputMessageDialog("Write photo story:");
+            bool result = await dlg.ShowAsync();
+            if (result != false)
+            {
+                string userInput = dlg.TextBox.Text;
+                TextBox description = new TextBox();
+                description.Text = userInput;
+                
+                //Insert text description to grid
+                description.SetValue(Grid.ColumnProperty, gridCollumToInsert);
+                description.SetValue(Grid.RowProperty, 1);
+                newGrid.Children.Add(description);
+               
+            }
 
-            
 
+            //myGrid.Children.Add(img_viewerN);
+            if (!lastImgOnLeft)
+            {
+                myStackpanel.Children.Add(newGrid);
+            }
+
+            //Negation variable with last image position
+            lastImgOnLeft = !lastImgOnLeft;
+           
 
 
         }
